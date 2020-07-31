82aa72d41e50af7038458ce98875d97647a0959c
unknown <danitzabonilla@gmail.com>
2020-07-30 19:56:59 -0500

    Updates

---
+++ precache-manifest.b09e5b9ab6a6eacf558e4882f35466c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3659e74d721fa21719479059c0ed978e",
+    "url": "/acamicaBloqueDos/index.html"
+  },
+  {
+    "revision": "e305158d7d9aeffe6db6",
+    "url": "/acamicaBloqueDos/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3d2534eba148d1d959b9",
+    "url": "/acamicaBloqueDos/static/js/2.fdc70579.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/acamicaBloqueDos/static/js/2.fdc70579.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e305158d7d9aeffe6db6",
+    "url": "/acamicaBloqueDos/static/js/main.3bb71bec.chunk.js"
+  },
+  {
+    "revision": "17b58903ec1e09ba40f8",
+    "url": "/acamicaBloqueDos/static/js/runtime-main.f021328a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/acamicaBloqueDos/static/media/logo.5d5d9eef.svg"
+  }
+]);
